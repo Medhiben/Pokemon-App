d8c3d4afb10d5150f611e842427bf2c4c539d659
Medhi Ramdani <m.ramdani@groupeonepoint.com>
2023-10-19 18:24:42 +0200

    project pokemon app

---
+++ src/app/mock-pokemon-list.ts
@@ -0,0 +1,112 @@
+import { Pokemon } from './pokemon';
+  
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: "Bulbizarre",
+        hp: 25,
+        cp: 5,
+        picture: "../assets/images/Bulbizarre.jpeg",
+        types: ["Plante", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 2,
+        name: "Salamèche",
+        hp: 28,
+        cp: 6,
+        picture: "../assets/images/Salamèche.jpeg",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 3,
+        name: "Carapuce",
+        hp: 21,
+        cp: 4,
+        picture: "../assets/images/Carapuce.jpeg",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Aspicot",
+        hp: 16,
+        cp: 2,
+        picture: "../assets/images/Aspicot.jpeg",
+        types: ["Insecte", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 5,
+        name: "Roucool",
+        hp: 30,
+        cp: 7,
+        picture: "../assets/images/Roucool.jpeg",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 6,
+        name: "Rattata",
+        hp: 18,
+        cp: 6,
+        picture: "../assets/images/Rattata.jpeg",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 7,
+        name: "Piafabec",
+        hp: 14,
+        cp: 5,
+        picture:"../assets/images/Piafabec.jpeg",
+        types: ["Normal", "Vol"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: "Abo",
+        hp: 16,
+        cp: 4,
+        picture: "../assets/images/Abo.jpeg",
+        types: ["Poison"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: "Pikachu",
+        hp: 21,
+        cp: 7,
+        picture: "../assets/images/Pikachu.jpeg",
+        types: ["Electrik"],
+        created: new Date()
+    },
+    {
+        id: 10,
+        name: "Sabelette",
+        hp: 19,
+        cp: 3,
+        picture:"../assets/images/Sabelette.jpeg",
+        types: ["Normal"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: "Mélofée",
+        hp: 25,
+        cp: 5,
+        picture: "../assets/images/Mélofée.jpeg",
+        types: ["Fée"],
+        created: new Date()
+    },
+    {
+        id: 12,
+        name: "Groupix",
+        hp: 17,
+        cp: 8,
+        picture: "../assets/images/Groupix.jpeg",
+        types: ["Feu"],
+        created: new Date()
+    }
+];
