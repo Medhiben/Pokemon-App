99f19f1a8a83c40a70c1a2e61a2e42d8dcceac93
Medhi Ramdani <m.ramdani@groupeonepoint.com>
2023-10-25 19:40:58 +0200

    add directive bordercolor and add module pokemon to centralize project and refactoriser project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,15 @@
-import { Component , OnInit} from '@angular/core';
-import { POKEMONS } from './mock-pokemon-list';
-import { Pokemon } from './pokemon';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl:  './app.component.html',
 })
-export class AppComponent implements OnInit {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon|undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string){
-    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+export class AppComponent {}
+ 
+  
     
-    if(pokemon){
-      console.log(`Vous avez demandé le Pokemon ${pokemon.name}`);
-      this.pokemonSelected = pokemon; 
-    }else{
-      console.log(`Vous avez demandé un Pokemon qui n'existe pas`);
-      this.pokemonSelected = pokemon; 
-    }
-
+ 
 
     
-  }
-}
 
 
